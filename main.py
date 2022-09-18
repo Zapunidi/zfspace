d83321f74a728b0cd97215738f6143ce3bcb56e8
Sergey Zapunidi <zapunidy@gmail.com>
2022-09-18 19:16:57 +0300

    Added some trailing help Added command lines arguments support, ZFS bridge class dummy

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+#!/usr/bin/env python3
+""" Usage: zfspace <pool or dataset name>
+
+Tool to analyze missing disk space by ZFS
+
+The main purpose is to visualize missing space that is hidden in snapshots.
+ZFS only shows space occupied by a snapshot's unique data and doesn't
+show space occupied by data referenced in 2+ snapshots. Therefore searching
+for missing space can be troublesome. zfspace helps with that and tries to be
+explanatory for inexperienced users.
+"""
+
 import os
+import sys
+
+
+class ZfsBridge:
+    def __init__(self):
+        pass
+
+    def get_snapshots(self, dataset_name):
+        pass
+
 
 class SnapshotSpace:
-    sizes_test = {100, 917020001, 2950810867664, 1417432010192, 63699569296, 31936161312, 180680896, 180680896}
+    sizes_test = {100, 917020001, 2950810867664, 1417432010192, 63699569296, 31936161312, 180680896, 180680897}
     term_format = dict(PURPLE='\033[95m', CYAN='\033[96m', DARKCYAN='\033[36m', BLUE='\033[94m',
                        GREEN='\033[92m', YELLOW='\033[93m', RED='\033[91m', BOLD='\033[1m',
                        UNDERLINE='\033[4m', END='\033[0m')
@@ -9,7 +31,8 @@ class SnapshotSpace:
     def __init__(self):
         self.term_columns, self.term_lines = os.get_terminal_size()
 
-    def _size2human(self, size):
+    @staticmethod
+    def _size2human(size):
         size_format = '{:.4}'
 
         if size < 0:
@@ -34,6 +57,9 @@ class SnapshotSpace:
         else:
             raise ValueError('Did not expect snapshot size to exceed 1000 pebibytes')
 
+    def print_used(self):
+        self._print_line(self.sizes_test)
+
     def _print_line(self, sizes):
         # Calculate fractional space for strings including len + 1 separators
         frac_size = (self.term_columns - len(sizes) - 1) / len(sizes)  # possibly non integer length
@@ -42,17 +68,15 @@ class SnapshotSpace:
             pos += frac_size
             length = int(pos)
             print('|', end='')
-            self._print_used(size, length - 1)
+            self._print_used(size, length)
             pos -= length
         print('|')  # New line afterwards
 
     def _print_used(self, used, str_length):
-        len = '{:d}'.format(str_length)
-        print('{:^12s} {}'.format(self._size2human(used), str_length), end='')
+        len_format = '{:^' + '{:d}'.format(str_length) + 's}'  # Prepare format string with desired width
+        print(len_format.format(self._size2human(used)), end='')
 
     def test(self):
-        for size in self.sizes_test:
-            print(self._size2human(size))
         print('columns = {}, lines = {}'.format(self.term_columns, self.term_lines))
         print(self.term_format['BOLD'] + 'Hello World !' + self.term_format['END'])
 
@@ -60,8 +84,11 @@ class SnapshotSpace:
 def main():
     ss = SnapshotSpace()
     ss.test()
-    ss._print_line(ss.sizes_test)
+    ss.print_used()
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) == 2:
+        main()
+    else:
+        sys.exit("Usage: {} <datasetname>".format(sys.argv[0]))
