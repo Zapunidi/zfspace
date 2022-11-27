11100a03d31b052e7f31251514915eed41059656
Sergey Zapunidi <zapunidy@gmail.com>
2022-11-27 12:48:03 +0300

    Added NULL test for future further development

---
+++ tests/test_zfspace.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+import os
+
+
+os.chdir(os.path.dirname(__file__) + '/..')
+
+
+class TestZfspace(TestCase):
+    def test_null(self):
+        pass
+        self.assertTrue(True)
